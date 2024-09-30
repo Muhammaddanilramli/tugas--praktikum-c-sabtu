85a2c3bc2369ce768e70bb088255290a7010f6fd
unknown <ramlidanil57@gmail.com>
2024-09-30 09:39:32 +0700

    tugas praktikum c sabtu

---
+++ Muhammmad danil ramli.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main ()
+{
+	printf("Panjang = 20 cm\n");
+	printf("Lebar = 17 cm\n");
+	
+	int P, b, L;
+	P = 20;
+	b = 17;
+	L = P * b;
+	printf("%i", L);
+	return 0;
+	
+}
---
+++ muhammad danil ramli 2.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	char nama[] = "muhammad danil ramli";
+	char nim[] = "2441030";
+	char jurusan[] = "teknik informatika";
+	
+	printf("NAMA: %s\n", nama);
+	printf("NIM: %s\n", nim);
+	printf("JURUSAN: %s\n", jurusan);
+	
+	return 0;
+}
